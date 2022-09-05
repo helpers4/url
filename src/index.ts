658d9d4ff4ac80d86b3424bf531983b0a2dd4ad8
Bérenger <berenger@arnaud.xyz>
2022-09-05 21:04:12 +0200

    chore: :memo: add new comments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+/*
+ * This program is under the terms of the GNU Affero General Public License version 3
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+/**
+ * Library of URL helpers.
+ *
+ * @module @helpers4/url
+ */
+
 export * from './helper/url';
